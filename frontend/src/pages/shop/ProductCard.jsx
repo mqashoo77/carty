8ca8520188d723d1b4c2f20c206fa055a3cd4009
Mohammad Qashoo <mqashoo77@gmail.com>
2024-11-25 15:41:45 +0200

    fix bug

--- frontend/src/pages/shop/ProductCard.jsx
+++ frontend/src/pages/shop/ProductCard.jsx
@@ -13,16 +13,15 @@ const ProductCards = ({ product }) => {
 
   return (
     <>
+    <div class="group border-gray-100/30 flex flex-col justify-between w-full max-w-xs h-100 self-center overflow-hidden rounded-lg border bg-white shadow-md ">
       <Link to={`/shop/${product._id}`}>
-        <div class="group border-gray-100/30 flex flex-col justify-between w-full max-w-xs h-100 self-center overflow-hidden rounded-lg border bg-white shadow-md ">
-          
-            <img
-              className="peer  top-0 right-0 h-full w-full object-cover"
-              src={product.image}
-              alt="product image"
-            />
         
-            
+          <img
+            className="peer  top-0 right-0 h-full w-full object-cover"
+            src={product.image}
+            alt="product image"
+          />
+
           <div className="mt-4 px-5 pb-5">
             <a href="#">
               <h5 class="text-xl tracking-tight text-black">{product.name}</h5>
@@ -34,17 +33,19 @@ const ProductCards = ({ product }) => {
               </p>
             </div>
             <RatingStars rating={product.rating} />
-            
           </div>
-
+          </Link>
           <div className="mt-4 px-5 pb-5">
-          <a class="hover:border-white/40 flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300">
+            <a onClick={(e) => {
+                            e.stopPropagation();
+                            handleAddToCart(product)
+                        }} className="hover:cursor-pointer hover:border-white/40 flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300">
               <ShoppingCartCheckoutOutlinedIcon className="p-0.5 text-white" />
               Add to cart
             </a>
           </div>
         </div>
-      </Link>
+      
     </>
   );
 };
